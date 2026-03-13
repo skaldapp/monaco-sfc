44328797fca8e32b83856b91bda9b755203d3510
Jerry Bruwes <jbruwes@icloud.com>
2026-03-13 13:11:59 +0200

    Update dependencies: @skaldapp/configs, @types/node, vite

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const path = "path-browserify",
   external = ["monaco-editor/esm/vs/editor/editor.worker"],
   fileName = "vue.worker",
   resolve = { alias },
-  rollupOptions = { external };
+  rolldownOptions = { external };
 
 export default mergeConfig(
   config,
@@ -16,7 +16,7 @@ export default mergeConfig(
     build: {
       emptyOutDir,
       lib: { entry, fileName, formats: ["es"] },
-      rollupOptions,
+      rolldownOptions,
     },
     resolve,
   }),
